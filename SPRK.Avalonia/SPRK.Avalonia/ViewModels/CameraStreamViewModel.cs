10e824bbc5714a7c5b15c2e7c4f7b47027c72ffe
kylewr <kyle@quackings.com>
2026-01-31 17:33:24 -0500

    status bar on cam window?

--- SPRK.Avalonia/SPRK.Avalonia/ViewModels/CameraStreamViewModel.cs
+++ SPRK.Avalonia/SPRK.Avalonia/ViewModels/CameraStreamViewModel.cs
@@ -1,5 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Globalization;
 
 namespace SPRK.Avalonia.ViewModels;
 
@@ -10,4 +14,35 @@ public partial class CameraStreamViewModel : ViewModelBase
 
     [ObservableProperty]
     private bool _isLoaded = false;
+
+    public string StatusText => IsLoaded ? "Connected" : "Connecting...";
+
+    public string FrameInfo => CurrentFrame is not null 
+        ? $"{CurrentFrame.PixelSize.Width} × {CurrentFrame.PixelSize.Height}" 
+        : "";
+
+    partial void OnIsLoadedChanged(bool value)
+    {
+        OnPropertyChanged(nameof(StatusText));
+    }
+
+    partial void OnCurrentFrameChanged(Bitmap? value)
+    {
+        OnPropertyChanged(nameof(FrameInfo));
+    }
+}
+
+public class BoolToColorConverter : IValueConverter
+{
+    public static readonly BoolToColorConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is true ? Colors.LimeGreen : Colors.Orange;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
 }
